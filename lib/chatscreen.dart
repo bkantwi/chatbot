4b235615cb92eee22011f1fa362f0174fd95926e
Bernard Antwi <bernardantwi2010@gmail.com>
2023-01-19 11:04:01 +0000

    closed on chatgpt api implementation

--- lib/chatscreen.dart
+++ lib/chatscreen.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:flutter/material.dart';
+import 'package:openaichat/threedots.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:openaichat/chatmessage.dart';
+import 'package:openaichat/threedots.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -14,18 +19,61 @@ class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
   final List<ChatMessage> _messages = [];
 
+  ChatGPT? chatGPT;
+
+  StreamSubscription? _subscription;
+  bool _isTyping = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  void _sendMessage() {
+    ChatMessage message = ChatMessage(text: _controller.text, sender: "user");
+    setState(() {
+      _messages.insert(0, message);
+      _isTyping = true;
+    });
+    _controller.clear();
+
+  final request = CompleteReq(
+      prompt: message.text, model: kTranslateModelV3, max_tokens: 200);
+
+  _subscription = chatGPT!.builder("", orgId: "")
+    .onCompleteStream(request: request)
+    .listen((response) {
+      Vx.log(response!.choices[0].text);
+      ChatMessage botMessage =
+          ChatMessage(text: response.choices[0].text, sender: "bot");
+
+      setState(() {
+        _isTyping = false;
+        _messages.insert(0, botMessage);
+      });
+  });
+
+  }
+
   Widget _buildTextComposer(){
     return Row(
       children: [
         Expanded(
           child: TextField(
             controller: _controller,
+            onSubmitted: (value) =>_sendMessage(),
             decoration: InputDecoration.collapsed(hintText: "What's on your mind?"),
           ),
         ),
         IconButton(
           icon: const Icon(Icons.send),
-          onPressed: () {},
+          onPressed: () => _sendMessage(),
         ),
       ],
     ).px16();
@@ -34,7 +82,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Chat Buddy'),),
+      appBar: AppBar(title: const Text('Chat Buddy'), centerTitle: true,),
       body: SafeArea(
         child: Column(
           children: [
@@ -46,6 +94,10 @@ class _ChatScreenState extends State<ChatScreen> {
                     itemBuilder: (context, index){
                       return _messages[index];
                     })
+            ),
+            if (_isTyping) const ThreeDots(),
+            const Divider(
+              height: 1.0,
             ),
             Container(
               decoration: BoxDecoration(
